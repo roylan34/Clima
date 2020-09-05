03e140831d6e999a07d03650f03ac704d4b4844a
Roelan Eroy <roelan@vanana.com>
2020-09-05 22:01:38 +0800

    add struct utility for computed properties

---
+++ Clima/Model/WeatherUtil.swift
@@ -0,0 +1,35 @@
+//
+//  WeatherMisc.swift
+//  Clima
+//
+//  Created by Roelan Geil Eroy on 9/5/20.
+//
+
+import Foundation
+
+struct WeatherUtil {
+    
+    let conditionId: Int
+    let cityName: String
+    let temperature: Double
+    
+    //Computed properties
+    var conditioName: String {
+        switch conditionId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 801...804:
+            return "cloud.fill"
+        default:
+            return "cloud"
+        }
+        
+    }
+    
+    var temperatureString: String{
+        return String(format: "%.0f",temperature);
+    }
+    
+}
